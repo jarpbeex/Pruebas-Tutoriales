184dc6f1b624679bf9c2241753f73fc4e1c15e8c
jarpbeex <josue_rodri_1342@hotmail.com>
2024-09-04 11:49:25 -0500

    avance angular tutorial

--- 7HTuto-sergieCode/src/app/app.routes.ts
+++ 7HTuto-sergieCode/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent }
+  {path: 'products', component: ProductsComponent }
+  {path: 'product*:id', component: ProductDetailComponent }
+  {path: 'contact', component: ContactComponent }
+  {path: '**', redirectTo: '', pathMatch: 'full' }
+];
